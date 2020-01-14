28b78b225b0350f4d8a7e2366e885ab69a66b0bb
Ricardo <jruales99@gmail.com>
2020-01-14 18:35:32 -0500

    2do

---
+++ banca/Models/Empresa.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace banca.Models
+{
+    public class Empresa
+    {
+        private string EmpCodigo { get; set; }
+        private string EmpNombre { get; set; }
+        private string EmpLeyenda { get; set; }
+        private string EmpDescripcion { get; set; }
+    }
+
+    public class EmpresaDBContext : DbContext
+    {
+        public DbSet<Empresa> Empresa { get; set; }
+    }
+}
