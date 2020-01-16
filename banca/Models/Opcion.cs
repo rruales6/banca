bac6c8347c0120e1d0056928b7dc62dd35cbf46b
Ricardo <jruales99@gmail.com>
2020-01-15 19:19:28 -0500

    20200115-1

---
+++ banca/Models/Opcion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace banca.Models
+{
+    public class Opcion
+    {
+        private int OpId { get; set; }
+        private string OpNombre { get; set; }
+        private string OpDescripcion { get; set; }
+        private int? OpPadreId { get; set; }
+        public virtual Opcion OpPadre { get; set; } 
+        public virtual ICollection<Opcion> OpHijos { get; set; }
+            
+
+    }
+
+    public class OpcionContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            HasKey(x => x.CategoryId);
+
+            Property(x => x.CategoryId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            HasOptional(x => x.Parent)
+                .WithMany(x => x.Children)
+                .HasForeignKey(x => x.ParentId)
+                .WillCascadeOnDelete(false);
+        }
+        public DbSet<Opcion> Opcion { get; set; }
+
+
+    }
+}   
